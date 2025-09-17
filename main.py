3b11fcca7407b8061d0f423404af7e0e6729bb31
Denis <densmm@ukr.net>
2025-09-17 12:24:52 +0300

    homework8

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
 # task1
 #
-# def reversed_generator(list):
-#     for i in range(len(list) - 1, -1, -1):
-#         yield list[i]
+# import random
 #
-# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# with open("nums.txt", "w") as f:
+#     for _ in range(1000):
+#         num = random.randint(1, 1000)
+#         f.write(f"{num}\n")
 #
-# for num in reversed_generator(nums):
-#     print(num)
-
-# task2
-
-# numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print("Файл random_numbers.txt створено та заповнено 10000 випадкових чисел.")
 #
-# squares=[x**2 for x in numbers if x % 2 == 0]
-# print(squares)
-
-
-# numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# with open("nums.txt", "r") as f:
+#     total_sum=sum(int(line.strip()) for line in f)
 #
-# squares=[]
-#
-# for num in numbers:
-#     if num%2==0:
-#         squares.append(num**2)
-#
-# print(squares)
+# print(f"Сума чисел у файлі: {total_sum}")
 
 # task3
 
-def prime_nums(n):
-    def is_prime(k):
-        if k < 2:
-            return False
-        for i in range(2, int(k**0.5)+1):
-            if k % i == 0:
-                return False
-        return True
-    count = 0
-    num=2
-    while count < n:
-        if is_prime(num):
-            yield num
-            count += 1
-        num += 1
+# import pickle
+# import json
+#
+# products=[
+#     {"id": 1, "name": "Ноутбук", "price": 25000, "in_stock": True},
+#     {"id": 2, "name": "Смартфон", "price": 15000, "in_stock": True},
+#     {"id": 3, "name": "Навушники", "price": 2000, "in_stock": False},
+#     {"id": 4, "name": "Клавіатура", "price": 1200, "in_stock": True}
+# ]
+#
+# with open("products.pkl", "wb") as f:
+#     pickle.dump(products, f)
+#
+# print("Список товарів збережено у форматі pickle (products.pkl)")
+#
+# with open("products.json", "w", encoding="utf-8") as products_file:
+#     json.dump(products, products_file, ensure_ascii=False, indent=4)
+#
+# print("Список товарів збережено у форматі JSON (products.json)")
 
-for ns in prime_nums(10):
-    print(ns)
+# task2
 
+import shelve
+
+with shelve.open("short_links_db") as short_links:
+    full_link = input("Enter the link: ")
+    short_link = input("Enter the short link: ")
+    short_links[short_link] = full_link
+    print(f"Short link '{short_link}' saved!")
+
+    search_link = input("Enter the link: ")
+    result = short_links.get(search_link)
+    if result:
+        print(f"Short link '{search_link}' found!")
+    else:
+        print(short_links.get(search_link), "404 Not found")
 
